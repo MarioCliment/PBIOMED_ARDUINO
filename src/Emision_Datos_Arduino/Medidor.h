e39dbe9b657788dfe2841e459074c353f747660a
Mario Climent Jiménez <mclijim@epsg.upv.es>
2023-10-31 11:43:27 +0100

    Adición en clase Medidor
    Añadidos a la clase medidor todos los métodos necesarios para poder utilizar el sensor correctamente.

--- src/Emision_Datos_Arduino/Medidor.h
+++ src/Emision_Datos_Arduino/Medidor.h
@@ -1,5 +1,12 @@
 // -*- mode: c++ -*-
 
+// --------------------------------------------------------------
+//
+// Creado por: Jordi Bataller i Mascarell (2019-07-07)
+// Modificado por: Mario Climent Jimenez  (2023-10-29)
+//
+// --------------------------------------------------------------
+
 #ifndef MEDIDOR_H_INCLUIDO
 #define MEDIDOR_H_INCLUIDO
 
@@ -7,9 +14,38 @@
 // ------------------------------------------------------
 class Medidor {
 
-  // .....................................................
-  // .....................................................
+// .....................................................
+// .....................................................
 private:
+
+//Definir pines
+enum PINES{
+  gasPin = 5;
+  refPin = 28;
+  tempPin = 29;
+} // enum
+
+//Funcion para normalizar valores
+// .....................................................
+// valor:float --> normalizar() --> float
+// .....................................................
+float normalizar(float valor){
+  float respuesta = (valor*3.3)/pow(2,12);
+  return respuesta;
+} // ()
+
+//Funcion para calcular la temperatura detectada por el sensor con la ecuacion del datasheet
+// .....................................................
+// valor:float --> calcularTemperatura() --> float
+// .....................................................
+float calcularTemperatura(float valor){
+  float temperatura = 87*valor-18;
+  if (temperatura>20) {
+    float correccion=(temperatura-20)*0.3;
+    temperatura = temperatura-correccion;
+  }
+  return temperatura;
+} // ()
 
 public:
 
@@ -20,21 +56,50 @@ public:
   } // ()
 
   // .....................................................
+  // iniciarMedidor()
   // .....................................................
+  //Establecemos los valores para los pines del sensor
   void iniciarMedidor() {
-	// las cosas que no se puedan hacer en el constructor, if any
+    pinMode(PINES::gasPin, INPUT);
+    pinMode(PINES::refPin, INPUT);
+    pinMode(PINES::tempPin, INPUT);
   } // ()
 
   // .....................................................
+  //  medirOzono() --> float
   // .....................................................
-  int medirCO2() {
-	return 235;
+  float medirOzono() {
+    //Guardar la lectura analogica en una variable
+    float vGas = analogRead(gasPin);
+    float vRef = analogRead(refPin);
+
+    //Normalizar la lectura anterior con la llamada a la funcion normalizar
+    float vGasN = normalizar(vGas);
+    float vRefN = normalizar(vRef);
+
+    //Calculamos vMedida
+    float vMedida = vGasN-vRefN;
+
+    //Calculamos la concentracion
+    float concentracion = vMedida/(-41.26*499*pow(10,-6));
+
+    return concentracion;
   } // ()
 
   // .....................................................
+  // medirTemperatura() --> float
   // .....................................................
-  int medirTemperatura() {
-	return -12; // qué frío !
+  float medirTemperatura() {
+    //Guardar la lectura analogica en una variable
+    float vTemp = analogRead(tempPin);
+
+    //Normalizar la lectura anterior con la llamada a la funcion normalizar
+    float vTempN = normalizar(vTemp);
+
+    //Calcular la temperatura detectada por el sensor con la ecuacion del datasheet
+    float temperatura = calcularTemperatura(vTempN)
+
+    return temperatura;
   } // ()
 	
 }; // class
